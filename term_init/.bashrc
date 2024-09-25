323779c0b117bd9eb72126d6973c75521411bce4
Md. Safirur Rashid <safirur@altersense.com>
2024-09-25 19:55:26 +0600

    init update. root notes added

--- term_init/.bashrc
+++ term_init/.bashrc
@@ -1,4 +1,5 @@
 ### oh my posh
 export PATH=$PATH:/home/tuf/bin
+# export PATH=$PATH:/root/.local/bin
 eval "$(oh-my-posh init bash --config https://raw.githubusercontent.com/5AF1/my_snippets/main/my_simple_posh.json)"
 ### end of oh my posh
--- term_init/config.fish
+++ term_init/config.fish
@@ -1,2 +1,3 @@
+### file probably located in ~/.config/fish/config.fish
 # set -Ux fish_user_paths /home/tuf/bin $fish_user_paths
 oh-my-posh init fish --config https://raw.githubusercontent.com/5AF1/my_snippets/main/my_simple_posh.json | source
